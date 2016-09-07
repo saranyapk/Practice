9af90802358faff2fbdf429e27b850b64d55f86c
saranyapk <saranyaponkumar@gmail.com>
2016-09-07 12:09:23 +0530

    Thread Practice

---
+++ src/com/saranyapk/practice/corejava/thread/ThreadPractice.java
@@ -0,0 +1,91 @@
+package com.saranyapk.practice.corejava.thread;
+
+public class ThreadPractice
+{
+	public static void main( String args[] )
+	{
+		Object lock = new Object();
+
+		Thread even = new Thread( new EvenThread( lock ) );
+		Thread odd = new Thread( new OddThread( lock ) );
+
+		even.start();
+		odd.start();
+	}
+}
+
+class EvenThread implements Runnable
+{
+	Object lock;
+
+	public EvenThread( Object lock )
+	{
+		this.lock = lock;
+	}
+
+	@Override
+	public void run()
+	{
+		for ( int i = 0; i < 10; i = i + 2 )
+		{
+			synchronized (lock)
+			{
+				lock.notify();
+
+				System.out.println( i );
+
+				try
+				{
+					lock.wait();
+				}
+				catch ( InterruptedException e )
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+}
+
+class OddThread implements Runnable
+{
+	Object lock;
+
+	public OddThread( Object lock )
+	{
+		this.lock = lock;
+	}
+
+	@Override
+	public void run()
+	{
+		try
+		{
+			Thread.sleep( 500 );
+		}
+		catch ( InterruptedException e1 )
+		{
+			e1.printStackTrace();
+		}
+		for ( int i = 1; i < 10; i = i + 2 )
+		{
+			synchronized (lock)
+			{
+				lock.notify();
+
+				System.out.println( i );
+
+				try
+				{
+					lock.wait();
+				}
+				catch ( InterruptedException e )
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+}
